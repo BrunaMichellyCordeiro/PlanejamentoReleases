807d1879ba7fbb32ef6c681ef6c6e99a166413e8
BrunaMichellyCordeiro <50689928+BrunaMichellyCordeiro@users.noreply.github.com>
2019-05-23 16:50:56 -0300

    Delete vogais.py

--- vogais.py
+++
@@ -1,8 +0,0 @@
-def ver_vogal(letra):
-
-    vogal = False
-
-    if letra == 'a' or letra == 'e' or letra == 'i' or letra == 'o' or letra == 'u' or letra == 'A' or letra == 'E' or letra == 'I' or letra == 'O' or letra == 'U':
-        vogal = True
-    else:
-        print("Não é vogal!")
